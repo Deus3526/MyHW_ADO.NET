232327a23e28e8ee308a8843357f395b59769fa2
柯冠宇 <qazwsxedc59764@gmail.com>
2022-04-14 15:49:40 +0800

    4/14/15:49:HW5

---
+++ MyHW/5. FrmAdventureWorks.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyHomeWork
+{
+    public partial class FrmAdventureWorks : Form
+    {
+        public FrmAdventureWorks()
+        {
+            InitializeComponent();
+            //在combobox添加年份
+            //方法1
+            //adventureWorksDataSet1.EnforceConstraints = false;
+            //productPhotoTableAdapter1.FillByYear(adventureWorksDataSet1.ProductPhoto);
+            //foreach (DataRow row in adventureWorksDataSet1.ProductPhoto.Rows)
+            //{
+            //    comboBox1.Items.Add(row["Year"]);
+            //}
+
+            ////方法2
+            SqlConnection connect = new SqlConnection("Data Source=.;Initial Catalog=AdventureWorks;Integrated Security=True");
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT year(ModifiedDate) as 'Year' FROM Production.ProductPhoto group by year(ModifiedDate) order by year(ModifiedDate)", connect);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+
+            //for (int i = 0; i < ds.Tables[0].Rows.Count; i++) comboBox1.Items.Add(ds.Tables[0].Rows[i][0]);
+            //or
+            foreach (DataRow row in ds.Tables[0].Rows) comboBox1.Items.Add(row["Year"]);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            productPhotoTableAdapter1.FillByDate(adventureWorksDataSet1.ProductPhoto,dateTimePicker1.Value,dateTimePicker2.Value);
+            SimpifyBindingUI();
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MoveFirst();
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MovePrevious();
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MoveNext();
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            bindingSource1.MoveLast();
+        }
+
+        private void bindingSource1_CurrentChanged(object sender, EventArgs e)
+        {
+            label4.Text = $"{ bindingSource1.Position + 1}/{bindingSource1.Count}";
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            productPhotoTableAdapter1.FillBySelectedYear(adventureWorksDataSet1.ProductPhoto, int.Parse(comboBox1.Text));
+            SimpifyBindingUI();
+        }
+        private void SimpifyBindingUI()
+        {
+            bindingSource1.DataSource = adventureWorksDataSet1.ProductPhoto;
+            bindingNavigator1.BindingSource = bindingSource1;
+            dataGridView1.DataSource = bindingSource1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            productPhotoTableAdapter1.FillByOderByDate(adventureWorksDataSet1.ProductPhoto);
+            SimpifyBindingUI();
+        }
+    }
+}
---
+++ MyHW/6. FrmMyAlbum_V1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace MyHW
+{
+    partial class FrmMyAlbum_V1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(589, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Tool CRUD";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(46, 105);
+            this.linkLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(29, 12);
+            this.linkLabel1.TabIndex = 1;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "台北";
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(46, 147);
+            this.linkLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(29, 12);
+            this.linkLabel2.TabIndex = 2;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "巴黎";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 57);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "My Album";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmMyAlbum_V1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 426);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FrmMyAlbum_V1";
+            this.Text = "FrmMyAlbum_V1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Label label1;
+    }
+}
